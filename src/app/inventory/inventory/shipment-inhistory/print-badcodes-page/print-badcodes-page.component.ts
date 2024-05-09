19ee1c10c25bef61967362a234989683a4522f5c
OdiZgb <84592093+OdiZgb@users.noreply.github.com>
2024-05-09 03:18:06 +0300

    Remove couting useless numbers under barcode

--- src/app/inventory/inventory/shipment-inhistory/print-badcodes-page/print-badcodes-page.component.ts
+++ src/app/inventory/inventory/shipment-inhistory/print-badcodes-page/print-badcodes-page.component.ts
@@ -23,42 +23,39 @@ export class PrintBadcodesPageComponent {
 
   printBarcode() {
     const pdf = new jspdf('p', 'mm', 'a4');
-
+  
     // Get the barcode container element
     const element = this.barcodeContainer.nativeElement;
-
+  
     // Loop through each barcode element and generate barcode image
     element.querySelectorAll('.barcode').forEach((barcodeElement: HTMLElement, index: number) => {
       // Get the value of the barcode
       const barcodeValue = localStorage.getItem("barcodeToPrint")+"";
-
+  
       // Generate the barcode image
       JsBarcode(barcodeElement, barcodeValue, {
         format: 'CODE128', // Specify barcode format if needed
         displayValue: false // Disable displaying the value next to the barcode
       });
-
-      // Add the number under the barcode
-      const barcodeNumber = document.createElement('div');
-      barcodeNumber.textContent = (index + 1).toString();
-      barcodeNumber.className = 'barcode-number';
-      barcodeElement?.parentNode?.appendChild(barcodeNumber);
     });
-
+  
     // Use html2canvas to capture the element as an image
-    html2canvas(element, { scale: 2, logging: false } as any).then((canvas) => {
+    html2canvas(element, { scale: 3, logging: false } as any).then((canvas) => {
       // Convert the canvas image to a data URL with higher quality
       const imageData = canvas.toDataURL('image/jpeg', 1.0);
-
+  
       // Calculate the PDF image dimensions
       const imgWidth = 190; // Adjust dimensions as needed
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
-
+  
       // Add the image to the PDF document
       pdf.addImage(imageData, 'JPEG', 10, 10, imgWidth, imgHeight);
-
+  
       // Save the PDF file
       pdf.save('barcode.pdf');
     });
   }
+  
+  
+  
 }
